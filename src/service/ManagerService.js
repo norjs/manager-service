883675c5fba5b01dc3f58d5fee000964289a405e
Jaakko Heusala <jheusala@iki.fi>
2019-08-01 11:03:46 +0300

    Updated ManagerService

--- src/service/ManagerService.js
+++ src/service/ManagerService.js
@@ -32,6 +32,8 @@ const HttpUtils = require('@norjs/utils/Http');
 
 // Types and interfaces
 require('@norjs/types/NorConfigurationObject.js');
+require('@norjs/types/NorManagerInstallActionObject.js');
+require('@norjs/types/NorManagerStartActionObject.js');
 
 /**
  *
@@ -58,6 +60,48 @@ class ManagerService {
          * @member {typeof ManagerService}
          */
         this.Class = ManagerService;
+
+        /**
+         *
+         * @type {{GET: {"/install/": (function(): {path: string}), "/start/": (function(): {path: string}), "/": (function(): {path: string})}}}
+         * @private
+         */
+        this._routes = {
+
+            GET: {
+
+                "/": {
+                    path: "/",
+                    actions: [
+                        {
+                            path: "/install/",
+                            method: "POST"
+                        },
+                        {
+                            path: "/start/",
+                            method: "POST"
+                        }
+                    ]
+                },
+
+                "/install/": {
+                    path: "/install/",
+                    method: "POST"
+                },
+
+                "/start/": {
+                    path: "/start/",
+                    method: "POST"
+                }
+
+            },
+
+            POST: {
+                "/install/": (req, res) => this._onInstallRequest(req, res),
+                "/start/": (req, res) => this._onStartRequest(req, res)
+            }
+
+        };
 
     }
 
@@ -88,13 +132,7 @@ class ManagerService {
 
         console.log(LogUtils.getLine(`Request "${req.method} ${req.url}" started`));
 
-        return HttpUtils.routeRequest(HttpUtils.getRequestAction(req), {
-            GET: {
-                "/": () => ({path: "index"}),
-                "/install/": () => ({path: "install"}),
-                "/start/": () => ({path: "start"})
-            }
-        });
+        return HttpUtils.routeRequest(HttpUtils.getRequestAction(req), this._routes, req, res);
 
     }
 
@@ -103,6 +141,36 @@ class ManagerService {
      */
     destroy () {
         console.log(LogUtils.getLine(`${ManagerService.getAppName()} destroyed`));
+    }
+
+    _onInstallRequest (req, res) {
+        return HttpUtils.getRequestDataAsJson(req).then(data => this._onInstallAction(data ? data.payload : {}));
+    }
+
+    _onStartRequest (req, res) {
+        return HttpUtils.getRequestDataAsJson(req).then(data => this._onStartAction(data ? data.payload : {}));
+    }
+
+    /**
+     *
+     * @param payload {NorManagerInstallActionObject}
+     * @private
+     */
+    _onInstallAction (payload) {
+        TypeUtils.assert(payload, "NorManagerInstallActionObject");
+        console.log(`WOOT: install action with `, payload);
+
+
+    }
+
+    /**
+     *
+     * @param payload {NorManagerStartActionObject}
+     * @private
+     */
+    _onStartAction (payload) {
+        TypeUtils.assert(payload, "NorManagerStartActionObject");
+        console.log(`WOOT: start action with `, payload);
     }
 
 }
